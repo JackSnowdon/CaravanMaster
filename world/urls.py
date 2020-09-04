5f4f3f08b2a7bd16690016b6018315c8463ee4ed
JackSnowdon <jacksnowdondrums@gmail.com>
2020-09-04 19:29:21 +0100

    back end button nav and editing locations

--- world/urls.py
+++ world/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('world_index/', world_index, name="world_index"),
     path('create_location/', create_location, name="create_location"),
+    path(r'edit_location/<int:pk>', edit_location, name="edit_location"),
 ]
--- world/views.py
+++ world/views.py
@@ -26,3 +26,30 @@ def create_location(request):
         loc_form = LocationForm()
     return render(request, "create_location.html", {"loc_form": loc_form})
 
+
+@login_required
+def edit_location(request, pk):
+    this_location = get_object_or_404(Location, pk=pk)
+    if request.user.profile.staff_access:
+        if request.method == "POST":
+            loc_form = LocationForm(request.POST, instance=this_location)
+            if loc_form.is_valid():
+                form = loc_form.save(commit=False)
+                form.save()
+                messages.error(
+                    request, "Edited {0}".format(form.name), extra_tags="alert"
+                )
+                return redirect("world_index")
+        else:
+            loc_form = LocationForm(instance=this_location)
+        return render(
+            request,
+            "edit_location.html",
+            {"loc_form": loc_form, "this_sheet": this_location},
+        )
+    else:
+        messages.error(
+            request, "You Don't Have The Required Permissions", extra_tags="alert"
+        )
+        return redirect("index")
+
